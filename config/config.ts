d1ffd1b5bf2734c8893b661239fd83cd33928097
Wuuuu <wuykjiu@163.com>
2021-07-07 18:50:16 +0800

    config: 修改配置

--- config/config.ts
+++ config/config.ts
@@ -1,5 +1,15 @@
 import { defineConfig } from 'umi';
+import defaultSettings from '@ant-design/pro-layout/lib/defaultSettings';
+console.log("🚀 ~ file: config.ts ~ line 3 ~ defaultSettings", defaultSettings)
 
 export default defineConfig({
   title: 'optimus prime',
+  theme: {
+    // ...darkTheme,
+    'primary-color': defaultSettings.primaryColor,
+  },
+  links: [
+    // href的图片你可以放在public里面，直接./图片名.png 就可以了，也可以是cdn链接
+     { rel: 'icon', href: '../public/favicon.png' },
+   ],
 });
